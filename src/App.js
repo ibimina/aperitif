70f7d358593b02af0b7bf3b2610e459ed43e46da
ibimina <96787210+ibimina@users.noreply.github.com>
2022-08-12 22:12:48 +0100

    remove empty css file and added comments

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
 
 import { useState } from "react";
 import { useEffect } from "react";
+
+//import routes from react dom
 import { Route, Routes } from "react-router-dom";
 import { BrowserRouter } from "react-router-dom";
-import "./App.css";
 
-import Navbar from "./components/Navbar";
+
+//import custom hook
 import { useTheme } from "./hooks/useTheme";
 
+//import component
 import Cocktail from "./pages/Cocktail";
 import Receipes from "./pages/Receipes";
 import Search from "./pages/Search";
-
+import Navbar from "./components/Navbar";
 
 
 function App() {
+
 const {color}= useTheme()
 const [showBtn,setShowBtn] = useState(false)
+
+//function to display scroll button 
 useEffect(()=>{
 window.addEventListener("scroll",()=>{
-  if (window.scrollY >400) {
+  if (window.scrollY > 400) {
     setShowBtn(true)
   } else {
   setShowBtn(false)  
@@ -27,10 +33,10 @@ window.addEventListener("scroll",()=>{
 })
 },[])
 
-
+//function to scroll page to the top
   const srol = () => {
     window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
-    console.log(document.doctype.scrollTop > 20);
+  
   };
   return (
     <div className="app">
@@ -46,6 +52,7 @@ window.addEventListener("scroll",()=>{
           <Route path="/" element={<Cocktail />} />
           <Route path="/cocktails/:id" element={<Receipes />} />
           <Route path="/search" element={<Search />} />
+          <Route path="*" element={<Cocktail />} />
         </Routes>
       </BrowserRouter>
     </div>
